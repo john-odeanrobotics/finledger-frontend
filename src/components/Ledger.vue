94aaa42f6a0386f6a32e55d4c657c0e68a9d6f26
john <john@odeanrobotics.com>
2021-09-14 13:47:34 +0900

    Ledger component

---
+++ src/components/Ledger.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="ledger">
+        <div>{{ledger.date}}</div>
+        <div>{{ledger.tag}}</div>
+        <div>{{ledger.memo}}</div>
+        <div><p v-if="ledger.isIncome">{{ledger.amount}}</p></div>
+        <div><p v-if="!ledger.isIncome">{{ledger.amount}}</p></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["ledger"]
+}
+</script>
+
+<style>
+#ledger{
+    display: flex;
+    justify-content: center;
+}
+</style>
