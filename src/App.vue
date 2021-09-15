871f02451972311d9195aeb012ab2a08eea2bdfd
john <john@odeanrobotics.com>
2021-09-15 10:30:09 +0900

    LedgerBoard Component

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,18 @@
 <template>
     <div id="app">
+        
         <h1>Financial Ledger</h1>
-        <ul class="ledger_board">
-            <li class="firstline">
-                <div>날짜</div>
-                <div>태그</div>
-                <div>메모</div>
-                <div>수입</div>
-                <div>지출</div>
-                <div>아이디</div>
-            </li>
-            <li v-for="ledger in ledgers" :key="ledger.id" v-bind:style="ledger.isIncome ? 'background-color: rgba(255, 0, 0, 0.25);' : 'background-color: rgba(0, 0, 255, 0.25)'">
-                <Ledger v-bind:ledger="ledger"></Ledger>
-            </li>
-        </ul>
+        <LedgerBoard></LedgerBoard>
     </div>
 </template>
 
 <script>
-import axios from "axios";
-import Ledger from "./components/Ledger.vue";
-const BaseUrl = "http://localhost:8080/api";
+import LedgerBoard from "./components/LedgerBoard.vue";
 
 export default {
     name: 'App',
-    data() {
-        return {
-            ledgers: []
-        }
-    },
-    async created() {
-        try {
-            const res = await axios.get(`${BaseUrl}/ledger`);
-            this.ledgers = res.data;   
-        } catch (error) {
-            console.error(error)
-        }
-    },
     components: {
-        Ledger
+        LedgerBoard
     }
 }
 </script>
@@ -55,6 +29,7 @@ export default {
 }
 .ledger_board{
     width: 70%;
+    min-width: 850px;
     padding: 0;
     list-style: none;
     margin: auto;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import VueRouter from "vue-router";
+import VueSession from "vue-session";
 import App from './App.vue'
+var options = {
+  persist: true
+}
 
+Vue.use(VueSession, options)
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false
