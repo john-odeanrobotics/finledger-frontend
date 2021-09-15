f89683d8935e73d753cb3ba9e06340909e7fea9b
john <john@odeanrobotics.com>
2021-09-15 10:30:28 +0900

    LedgerBoard Component

---
+++ src/components/LedgerBoard.vue
@@ -0,0 +1,45 @@
+<template>
+    <ul class="ledger_board">
+        <li class="firstline">
+            <div>날짜</div>
+            <div>태그</div>
+            <div>메모</div>
+            <div>수입</div>
+            <div>지출</div>
+            <div>아이디</div>
+        </li>
+        <li v-for="ledger in ledgers" :key="ledger.id" v-bind:style="ledger.isIncome ? 'background-color: rgba(255, 0, 0, 0.25);' : 'background-color: rgba(0, 0, 255, 0.25)'">
+            <Ledger v-bind:ledger="ledger"></Ledger>
+        </li>
+    </ul>
+</template>
+
+<script>
+import axios from "axios";
+import Ledger from "./Ledger.vue";
+const BaseUrl = "http://localhost:8080/api";
+
+export default {
+    name: "LedgerBoard",
+    data() {
+        return {
+            ledgers: []
+        }
+    },
+    async created() {
+        try {
+            const res = await axios.get(`${BaseUrl}/ledger`);
+            this.ledgers = res.data;   
+        } catch (error) {
+            console.error(error)
+        }
+    },
+    components: {
+        Ledger
+    }
+}
+</script>
+
+<style>
+
+</style>
